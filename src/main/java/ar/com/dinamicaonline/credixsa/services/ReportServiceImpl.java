41bca194c04d75dba9818dfd2de46326ddf113c8
AgustinMontenegro95 <agustinmontenegroam10@gmail.com>
2023-03-03 18:14:19 -0300

    Se avanzó en las respuestas.

--- src/main/java/ar/com/dinamicaonline/credixsa/services/ReportServiceImpl.java
+++ src/main/java/ar/com/dinamicaonline/credixsa/services/ReportServiceImpl.java
@@ -3,9 +3,6 @@ package ar.com.dinamicaonline.credixsa.services;
 import java.util.HashMap;
 import java.util.Map;
 
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
-import org.json.simple.parser.ParseException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
